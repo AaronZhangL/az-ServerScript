dd3b09d5efc354074929380b71840d012130eff6
AaronZhangL <aaronzhang0@gmail.com>
2015-06-06 20:54:39 +0900

    Add tools installation files.

---
+++ toolsList.sh
@@ -0,0 +1,9 @@
+#===========================================#
+#== Clone site with deirctories structrue ==#
+#===========================================#
+wget -m -e robots=off -U "Mozilla/5.0 (Windows; U; Windows NT 5.1; zh-CN; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6" "http://downloads2.goautodial.org/centos/5/"
+
+
+
+
+
