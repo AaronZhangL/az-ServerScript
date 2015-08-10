9b29c6f33f893c207f8a04703fae9a2093d7c670
AaronZhangL <aaronzhang0@gamil.com>
2015-08-10 18:32:37 +0900

    Add jre 64bit download url.

--- centOS_Install_Java.sh
+++ centOS_Install_Java.sh
@@ -1,6 +1,9 @@
-#For 64bit
+#For 64bit JDK
 cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
+
+#For 64bit JRE
+#http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jre-8u51-linux-x64.tar.gz
 tar xzf jdk-8u45-linux-x64.tar.gz
 
 #For 32bit JDK
