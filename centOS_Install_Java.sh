b5cda8138b0e4897d9bc60446cfd2149de875fb3
AaronZhangL <aaronzhang0@gamil.com>
2015-08-10 18:28:32 +0900

    Add jre 32bit download url.

--- centOS_Install_Java.sh
+++ centOS_Install_Java.sh
@@ -3,9 +3,14 @@ cd /opt/
 wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-x64.tar.gz"
 tar xzf jdk-8u45-linux-x64.tar.gz
 
-#For 32bit
+#For 32bit JDK
 #cd /opt/
 #wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u45-b14/jdk-8u45-linux-i586.tar.gz"
+
+#For 32bit JRE
+#wget --no-cookies --no-check-certificate --header "Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" "http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jre-8u51-linux-i586.tar.gz"
+#http://download.oracle.com/otn-pub/java/jdk/8u51-b16/jre-8u51-linux-i586.tar.gz
+
 #tar xzf jdk-8u45-linux-i586.tar.gz
 
 #Install Java with Alternatives
