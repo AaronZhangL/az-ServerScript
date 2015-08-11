98be600a45174a0dce0c16581a344a33663cc081
AaronZhangL <aaronzhang0@gamil.com>
2015-08-11 17:01:32 +0900

    Add mount sabma folder command

--- mountShareFolder.sh
+++ mountShareFolder.sh
@@ -4,3 +4,6 @@
 echo "Mount shared folder to [/mnt/disk2/02_shareFolder]."
 
 mount -t vboxsf -o uid=amiroot,gid=amiroot 02_shareFolder /mnt/disk2/02_shareFolder
+
+## Mount samba folder
+sudo mount -t cifs -o username=[UserName],password=[Password] //xxx.xxx.xxx.xxx/bak/ /mnt/backupServer
