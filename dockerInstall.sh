8e51e77791b101a696be34417467c83e7126be1c
AaronZhangL <aaronzhang0@gamil.com>
2016-01-05 17:08:47 +0900

    Add docker installation script.

---
+++ dockerInstall.sh
@@ -0,0 +1,8 @@
+# See: https://nodejs.org/en/download/package-manager/#enterprise-linux-and-fedora
+#Alternatively for Node.js v5:
+yum clean all
+curl --silent --location https://rpm.nodesource.com/setup_5.x | bash -
+yum -y install nodejs
+
+
+
